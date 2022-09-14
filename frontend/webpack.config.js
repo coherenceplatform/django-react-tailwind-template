5d7dd1114a2338945bc3f68fdc4aa34945a797cc
Zachary Zaro <zach@withcoherence.com>
2022-09-14 23:50:10 +0000

    fix FE build

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "./static/frontend"),
+    path: path.resolve(__dirname, "./build"),
     filename: "[name].js",
   },
   stats: {
